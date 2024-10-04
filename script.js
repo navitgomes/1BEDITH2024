0681368cc3f29cc8be8c0b5552cb71793377f5ff
Nathália Vitória Gomes <vitoria.gomes.nathalia@escola.pr.gov.br>
2024-10-04 08:54:54 -0300

    aula 1

---
+++ script.js
@@ -0,0 +1,16 @@
+//Julie and the Phantoms
+//Continência ao Amor
+//Minha Culpa
+//F4 Tailândia: Garotos Sobre Flores
+//A Cinco Passos de Você
+//A Culpa É das Estrelas
+//Cobra Kai
+//Diários de um Vampiro
+
+function setup() {
+    createCanvas(480, 280);
+  }
+  
+  function draw() {
+    background(100);
+  }
